bba613c251805eb80215444f2ed1d9174d95e790
Cam Caswell <camcaswell@gmail.com>
2018-04-26 23:09:12 -0400

    added __name__=='__main__' to lonely_songs.py

--- lonely_songs.py
+++ lonely_songs.py
@@ -2,26 +2,28 @@ import spotipy
 from spotipyhelper import *
 
 
-token = generate_token('''
-    user-library-read
-    playlist-read-private
-    playlist-modify-private
-    ''')
+if __name__ == '__main__':
+    
+    token = generate_token('''
+        user-library-read
+        playlist-read-private
+        playlist-modify-private
+        ''')
 
-sp = subSpotify(auth=token)
+    sp = subSpotify(auth=token)
 
-lonely_songs = sp.lonely_songs()
+    lonely_songs = sp.lonely_songs()
 
 
-print(*[s['name'] for s in lonely_songs], sep='\n')
-print(f"You have {len(lonely_songs)} lonely songs")
+    print(*[s['name'] for s in lonely_songs], sep='\n')
+    print(f"You have {len(lonely_songs)} lonely songs")
 
-if len(lonely_songs) > 0:
+    if len(lonely_songs) > 0:
 
-    new_playlist = sp.user_playlist_create(
-        user=sp.me()['id'],
-        name='All the Lonely Songs',
-        public=False
-        )
+        new_playlist = sp.user_playlist_create(
+            user=sp.me()['id'],
+            name='All the Lonely Songs',
+            public=False
+            )
 
-    sp.add_tracks_to_playlist(playlist=new_playlist, track_list=lonely_songs)
+        sp.add_tracks_to_playlist(playlist=new_playlist, track_list=lonely_songs)
