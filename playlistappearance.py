4e56b83a3af05fa10a858cd27a3075c1af91f342
Unknown <camcaswell@gmail.com>
2018-05-04 00:34:33 -0400

    QoL changes and better error handling

--- playlistappearance.py
+++ playlistappearance.py
@@ -16,15 +16,29 @@ if __name__ == '__main__':
 		print("Failed to get token.")
 		quit()
 
-	song_uri = input("Paste song URI here: ")
+	song_uri = input("\nPaste song URI here: ")
+
 	try:
-		song = sp.track(song_uri)
-	except (spotipy.SpotifyException):
-		print("Invalid URI")
-		quit()
+		while (song_uri != "x"):
 
-	playlists = sp.playlists_where_song_appears(sp.me()['id'], song['id'])
+			try:
+				song = sp.track(song_uri)
 
-	print(f"\n{song['name']} appears in: \n")
-	for playlist in playlists:
-		print(playlist['name'])
+				playlists = sp.playlists_where_song_appears(sp.me()['id'], song['id'])
+
+				if playlists:
+					print(f"\n{song['name']} appears in: \n")
+					for playlist in playlists:
+						print(f"* {playlist['name']}")
+
+				else:
+					print(f"\n{song['name']} doesn't appear in any of your playlists.")
+
+			except (spotipy.SpotifyException):
+				print("Invalid URI")
+
+			song_uri = input("\nPaste song URI here: ")
+
+	except Exception as error:
+		print(f"something went wrong:\n{error}")
+		input()
